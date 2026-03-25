f7419fda051dcf5b595ee4eb0d49295a194264d8
achackerbutty <achackerbutty@gmail.com>
2026-03-25 18:21:38 -0500

    Create route.ts

---
+++ app/api/leads/route.ts
@@ -0,0 +1,17 @@
+import { prisma } from "@/lib/prisma"
+import { NextResponse } from "next/server"
+
+export async function POST(req: Request) {
+  const body = await req.json()
+
+  const lead = await prisma.lead.create({
+    data: body
+  })
+
+  return NextResponse.json(lead)
+}
+
+export async function GET() {
+  const leads = await prisma.lead.findMany()
+  return NextResponse.json(leads)
+}
